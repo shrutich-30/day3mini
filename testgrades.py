b04e6ec4cfc3b8c1e1c490f440e3721e01e55442
shrutich-30 <shrutichaurasiya201@gmail.com>
2026-06-10 23:06:51 +0530

    Fix flake8 formatting

--- testgrades.py
+++ testgrades.py
@@ -1,16 +1,21 @@
 from grades import calculate_grade
 
+
 def test_grade_a():
     assert calculate_grade(95) == "A"
+
 
 def test_grade_b():
     assert calculate_grade(80) == "B"
 
+
 def test_grade_c():
     assert calculate_grade(65) == "C"
 
+
 def test_grade_d():
     assert calculate_grade(45) == "D"
+
 
 def test_grade_f():
     assert calculate_grade(20) == "F"
