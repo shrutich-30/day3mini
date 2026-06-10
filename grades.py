3fe257e7f63c95b8f015dacc9f2d77ca06bc13d9
shrutich-30 <shrutichaurasiya201@gmail.com>
2026-06-10 22:58:13 +0530

    Student Grade Checker CI Pipeline

---
+++ grades.py
@@ -0,0 +1,16 @@
+def calculate_grade(marks):
+
+    if marks >= 90:
+        return "A"
+
+    elif marks >= 75:
+        return "B"
+
+    elif marks >= 60:
+        return "C"
+
+    elif marks >= 40:
+        return "D"
+
+    else:
+        return "F"
